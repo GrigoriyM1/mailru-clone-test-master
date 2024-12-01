f956f950de81ba8f61f99e98efacf3fbe56b8b20
GrigoriyM1 <alek9051@gmail.com>
2024-12-01 18:08:49 +0400

    daadssad

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
-import * as cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser'
 import * as express from 'express'
 import { join } from 'path'
 import { AppModule } from './app.module'
