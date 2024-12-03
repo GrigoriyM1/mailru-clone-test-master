3e1206489fcc5ac1f16da7db3d0c482c850a9379
GrigoriyM1 <alek9051@gmail.com>
2024-12-03 20:38:17 +0400

    adsads

--- dddd/src/app.module.ts
+++ dddd/src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
+  imports: [
+    // MailerModule.forRoot({
+    //   transport: {
+    //     host: process.env.SMTP_HOST,
+    //     port: Number(process.env.SMTP_PORT),
+    //     secure: true,
+    //     auth: {
+    //       user: process.env.SMTP_USER,
+    //       pass: process.env.SMTP_PASSWORD,
+    //     },
+    //   },
+    //   defaults: {
+    //     from: `"No Reply" <${process.env.SMTP_USER}>`,
+    //   },
+    // }),
+    ConfigModule.forRoot({
+      isGlobal: true
+    }), 
+  ],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  // providers: [],
 })
 export class AppModule {}
