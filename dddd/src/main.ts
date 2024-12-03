08ff17de8c340203ec759dfe06648e2008e4b8ad
GrigoriyM1 <alek9051@gmail.com>
2024-12-03 20:36:50 +0400

    adsads

--- dddd/src/main.ts
+++ dddd/src/main.ts
@@ -1,33 +1,39 @@
-import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as express from 'express';
-import { join } from 'path';
-import { AppModule } from './app.module';
-import { ResponseAddContentRangeToHeaderInterceptor } from './interceptors/response-add-content-range.interceptor';
+import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import * as express from 'express'
+// import cookieParser from 'cookie-parser'; // добавить в tsconfig.json - "esModuleInterop": true
+import { join } from 'path'
+import { AppModule } from './app.module'
+import { ResponseAddContentRangeToHeaderInterceptor } from './interceptors/response-add-content-range.interceptor'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    rawBody: true,
-  });
+	const app = await NestFactory.create(AppModule, {
+		rawBody: true // чтобы handleWebhook работал
+	})
 
-  app.enableCors({
-    origin: [process.env.CLIENT_URL], 
-    credentials: true,
-  });
+	// app.setGlobalPrefix('api')
+	// app.use(cookieParser())
+	app.enableCors({
+		origin: [process.env.CLIENT_URL],
+		credentials: true,
+		// exposedHeaders: ['set-cookie'],
+	})
 
-  app.use('/webhook', express.raw({ type: '*/*' }));
+	// app.use('/uploads', express.static(join(__dirname, '..', 'uploads')))	
+	app.use('/webhook', express.raw({ type: '*/*' })) // Это позволит получать raw body
 
-  const config = new DocumentBuilder()
-    .setTitle('Mailru clone example')
-    .setDescription('Тесты api проекта')
-    .setVersion('1.0')
-    .addTag('mailru')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+	// swagger
+	const config = new DocumentBuilder()
+		.setTitle('Mailru clone example')
+		.setDescription('Тесты api проекта')
+		.setVersion('1.0')
+		.addTag('mailru')
+		.build()
+	const document = SwaggerModule.createDocument(app, config)
+	SwaggerModule.setup('docs', app, document)
 
-  app.useGlobalInterceptors(new ResponseAddContentRangeToHeaderInterceptor());
+	app.useGlobalInterceptors(new ResponseAddContentRangeToHeaderInterceptor())
 
-  await app.listen(process.env.PORT! || 4200);
+	await app.listen(process.env.PORT! || 4200)
 }
-bootstrap();
+bootstrap()
